315658b5f5a6dffa2890ef529b659c99fc1d9047
Az <hereliesaz@gmail.com>
2026-08-03 07:53:23 -0500

    feat: initialize MainActivity as the entry point for the Graffux editor application

--- app/src/main/java/com/hereliesaz/graffux/MainActivity.kt
+++ app/src/main/java/com/hereliesaz/graffux/MainActivity.kt
@@ -24,7 +24,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Surface
-import com.hereliesaz.graffitixr.design.components.FloatingWindow
 import androidx.compose.material3.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.RoundedCornerShape
